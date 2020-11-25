f46fa8d9d939d475faf8609811febf70b6185747
Rayapudi <A7536301@bestbuy.com>
2020-11-24 21:13:09 -0600

    first commit with Button and Test controls

---
+++ ButtonControl/Button/control.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { IIconProps, PrimaryButton, DefaultButton, initializeIcons, getTheme, Label, Stack } from 'office-ui-fabric-react';
+initializeIcons();
+export interface IScriptButtonProps {
+    primary?: boolean,
+    text: string,
+    disabled?: boolean,
+    fabricIcon: string,
+    onClick: () => void
+}
+
+export const ScriptButton: React.FunctionComponent<IScriptButtonProps> = props => {
+    const buttonIcon: IIconProps = { iconName: props.fabricIcon };
+    const theme = getTheme();   
+    return (
+        <DefaultButton primary={props.primary} text={props.text} disabled={props.disabled} onClick={props.onClick} iconProps={buttonIcon} />
+    );
+}
